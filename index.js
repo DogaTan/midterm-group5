7c8876828f36ad809b3d9a7e309696d4fe44fcce
DogaTan <doga.tannn@gmail.com>
2025-04-24 21:44:17 +0300

    feature(hello): add /hello endpoint

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/hello', (req, res) => {
+    res.send('🟢 Hello from Midterm API!');
+  });
+
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
   });
